0944bbfd07765d5db0a68a4b3c76e589fdd66f49
villa <s.a.villa.dev@gmail.com>
2024-03-18 11:43:03 +0100

    Initial commit

---
+++ app/src/main/java/com/sergio/examenfinal_damd_pmdm/ui/task/TaskActivity.kt
@@ -0,0 +1,12 @@
+package com.sergio.examenfinal_damd_pmdm.ui.task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sergio.examenfinal_damd_pmdm.R
+
+class TaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
